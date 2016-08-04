a3b4a71603b3139250e8988892f2c92a40c50026
Dmitry Galyuk <dmitry@galyuk.com>
2016-08-04 14:49:45 +0530

    server fix

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(80, function(){
-    console.log('Server running on 80...');
+var port = 80;
+connect().use(serveStatic(__dirname)).listen(port, function(){
+    console.log('Server running on ' + port + '...');
 });
