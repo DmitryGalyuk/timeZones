bbbb29d2f65b9709ec5aa2fb52f9e544ed35c2bf
Dmitry Galyuk <dmitry@galyuk.com>
2016-08-04 18:45:04 +0530

    port fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var serveStatic = require('serve-static');
 var connect = require('connect');
-var port = 8080;
+var port = 80;
 connect().use(serveStatic(__dirname)).listen(port, function(){
     console.log('Server running on ' + port + '...');
 });
